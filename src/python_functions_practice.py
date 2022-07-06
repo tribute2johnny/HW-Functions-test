96600033662cc9a3db9591883e5caeef586922ff
tribute2johnny <87375424+tribute2johnny@users.noreply.github.com>
2022-07-06 19:18:12 +0100

    Add files via upload

---
+++ src/python_functions_practice.py
@@ -0,0 +1,49 @@
+def return_10():
+    return 10
+
+def add(one, two):
+    return one + two
+    
+def subtract(ten, five):
+    return ten - five
+
+    
+def multiply(four, two):
+    return four * two
+
+def divide(ten, two):
+    return ten/two
+
+def length_of_string(string_length):
+    return len(string_length)
+
+def join_string(string_1, string_2):
+    return string_1 + string_2
+
+def add_string_as_number(num1, num2):
+    return int(num1) + int(num2)
+
+def number_to_full_month_name(num):
+    if num == 1:
+        return "January"
+        
+
+def number_to_full_month_name(num):
+    if num == 3:
+        return "March"
+
+def number_to_full_month_name(num):
+    if num == 9:
+        return "September"
+
+def number_to_short_month_name(num):
+    if num == 1:
+        return "Jan"
+
+def number_to_short_month_name(num):
+    if num == 4:
+        return "Apr"
+
+def number_to_short_month_name(num):
+    if num == 10:
+        return "Oct"y
